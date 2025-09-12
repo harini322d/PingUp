252f1ff0498d9cb33646ac2585c79ea20c48c2c8
Harini <m.harini8460@gmail.com>
2025-09-12 16:19:00 +0530

    backend complete

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,19 +1,22 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from "../controllers/userController.js";
+import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, getUserProfiles, sendConnectionRequest, unfollowUser, updateUserData } from "../controllers/userController.js";
 import { upload } from "../configs/multer.js";
+import { getUserRecentMessages } from "../controllers/messageController.js";
 
 const userRouter = express.Router();
 userRouter.get('/data',protect,getUserData)
 
 userRouter.post('/update',upload.fields([{name:'profile',maxCount:1},{name:'cover',maxCount:1}]),protect,updateUserData)
-
+ 
 userRouter.post('/discover',protect,discoverUsers)
 userRouter.post('/follow',protect,followUser)
 userRouter.post('/unfollow',protect,unfollowUser)
 userRouter.post('/connect',protect,sendConnectionRequest)
 userRouter.post('/accept',protect,acceptConnectionRequest)
 userRouter.get('/connections',protect,getUserConnections)
+userRouter.post('/profiles',getUserProfiles)
+userRouter.get('/recent-messages',protect,getUserRecentMessages)
 
 
 export default userRouter
